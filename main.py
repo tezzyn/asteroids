575cd7146b41ef98a670138f88b8fe5796334f05
tezzytech <qurteznealy@gmail.com>
2025-04-04 22:57:24 -0400

    starting asteroids

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import *
+
+def main():
+
+  pygame.init()
+
+  #SCREEN_WIDTH = constants.SCREEN_WIDTH
+  #SCREEN_HEIGHT = constants.SCREEN_HEIGHT
+
+  #print("Starting Asteroids!")
+  #print(f"Screen width: {SCREEN_WIDTH}")
+  #print(f"Screen height: {SCREEN_HEIGHT}")
+
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  while True:
+    screen.fill("black")
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        return
+
+
+if __name__ == "__main__":
+     main()
